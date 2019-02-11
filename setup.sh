f609b5c38fe2a2082ad0f5e0bd18b1004ebaaca8
Yan <yans@yancomm.net>
2019-02-11 14:50:45 -0700

    admin dir to path

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/scripts
+ADMIN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/admin
 
 if ! which pod_names >/dev/null
 then
@@ -8,5 +9,6 @@ then
 	cat <<END >> ~/.bashrc
 # kuboid
 export PATH=$SCRIPT_DIR:\$PATH
+export PATH=$ADMIN_DIR:\$PATH
 END
 fi
